ecf1ad965f9b3627a8263931aa06966ff1977c58
FAMASoon <famasoon@gmail.com>
2023-09-10 19:56:40 +0900

    fix: import path

--- api-lesson/src/app.module.ts
+++ api-lesson/src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './user/auth/auth.module';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { TodoModule } from './todo/todo.module';
